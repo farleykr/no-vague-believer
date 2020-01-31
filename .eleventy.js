6e98e4bfe6ccc189ebf4cc9558ff406843cb5447
Farley Reynolds <farley.k.reynolds@nasa.gov>
2020-01-31 13:34:36 -0500

    initial commit

---
+++ .eleventy.js
@@ -0,0 +1,18 @@
+const moment = require('moment');
+ 
+moment.locale('en');
+
+module.exports = function(eleventyConfig) {
+
+  eleventyConfig.addFilter('dateIso', date => {
+    return moment(date).toISOString();
+  });
+
+  eleventyConfig.addFilter('dateReadable', date => {
+    return moment(date).utc().format('MMMM Do, YYYY'); // E.g. May 31, 2019
+  });
+
+  // Copy `static/` to `_site/static`
+  // If you use a subdirectory, it’ll copy using the same directory structure.
+  eleventyConfig.addPassthroughCopy("static/");
+};
